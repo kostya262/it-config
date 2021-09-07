c3ad037e229c60c8ef186c9ac9fb1a030b5bcba0
Kostya <kostya.saharoff262@yandex.ru>
2021-09-07 10:29:54 +0300

    Add header on index page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,18 +2,20 @@
 @section('content')
 <header class="header">
   <div class="container header__container">
-    <button class="btn btn--primary header__new-task-btn">Create task</button>
+    <button class="btn btn--small btn--primary header__new-task-btn">Create task</button>
     <div class="header__sort-box">
       <label class="header__sort-text" for="headerSelect">Sort by</label>
       <select name="headerSelect" id="headerSelect" class="select header__sort-select">
         <option value="1">Срок выполнения</option>
       </select>
     </div>
-    <div class="header__user-box">
-      <span id="headerUserAvatar" class="header__user-avatar">EK</span>
-      <span id="headerUserName" class="header__user-name">{{ Auth::user()->name }}</span>
-    </div>
-    <a class="btn header__logout-btn"
+    <article class="header__user-box user">
+      <span class="user__avatar">
+        @php
+      </span>
+      <span class="user__name">{{ Auth::user()->name }}</span>
+    </article>
+    <a class="btn btn--small header__logout-btn"
     href="{{ route('logout') }}"
     onclick="event.preventDefault();
                   document.getElementById('logout-form').submit();">
@@ -24,5 +26,24 @@
     </form>
   </div>
 </header>
+
+<main class="index">
+  <section class="todo">
+    <div class="container">
+      <ul class="todo__list">
+        <li class="todo__item">
+          <article class="task-card">
+            <div class="task-card__users-list">
+              <div class="task-card__main-user">
+
+              </div>
+
+            </div>
+          </article>
+        </li>
+      </ul>
+    </div>
+  </section>
+</main>
 @endsection
 
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -16,8 +16,9 @@
 
     <!-- Styles -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/choices.js/public/assets/styles/choices.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/choices.js/public/assets/styles/base.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
+    {{-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/choices.js/public/assets/styles/base.min.css"> --}}
+    {{-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css"> --}}
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 <body>
